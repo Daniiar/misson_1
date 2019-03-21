e9f78edb35388d5b7a768316623b51cfd73e5777
Daniiar <lakiwolf.90@mail.ru>
2019-03-22 00:38:01 +0600

    added method for filtering keys for writing to file

--- file_handler.rb
+++ file_handler.rb
@@ -1,19 +1,16 @@
 require 'json'
 require 'pry'
 require 'net/http'
+require 'colorize'
+require 'colorized_string'
 
 class FileHandler
+  
+  Url = URI('https://raw.githubusercontent.com/Daniiar/My-JSON-Server/master/10_users.json')
+
   def self.read(file)
-    string = ''
-    data = File.readlines(file)
-    
-    data.each do |elem|
-      elem = elem.strip
-      string = string + elem
-    end
-    
-    new_hash = JSON.parse(string)
-    # puts new_hash
+    puts "\nFile reading...".light_yellow.on_magenta
+    puts "#{File.readlines(file)}".light_yellow.on_blue
   end
 
   def self.write(data)
@@ -32,37 +29,43 @@ class FileHandler
   end
 
   def self.filterKeys(hash)
-    binding.pry
-    # white_list = {hash[":numSpecs"]}
+    white_list = {}
+    white_list.store("id", hash["id"])
+    white_list.store("name", hash["name"])
+    white_list.store("username", hash["username"])
+    white_list.store("phone", hash["phone"])
+    white_list.store("website", hash["website"])
+    return white_list
+  end
+
+  def self.call
+    info = getData(Url)
+    info = filterKeys(info)
+    write(info)
+    puts "From url address...".light_yellow.on_magenta
+    print "#{memoizate(info)}".light_yellow.on_blue
   end
 
 end
 
-Url = URI('https://api.apis.guru/v2/metrics.json')
-
-FileHandler .read("incoming_file.json")
-animal_names = {lion: 'John', cat: 'Pusik', dog: 'Tom'}
-info = FileHandler.getData(Url)
-FileHandler.filterKeys(info)
-# binding.pry
-FileHandler.write(animal_names)
+FileHandler.call
+FileHandler.read('output.json')
 
 # Написать класс, в котором есть следующие методы:
 
-# - Чтение из JSON файла, парсинг json. Возвращает объект ruby
+# + Чтение из JSON файла, парсинг json. Возвращает объект ruby
 
-# - Запись в файл, принимает Hash, сериализирует полученные данные и записывает в локальный файл перезаписывая существующий файл и создав новый.
+# + Запись в файл, принимает Hash, сериализирует полученные данные и записывает в локальный файл перезаписывая существующий файл и создав новый.
 
-# - Метод в котором присваивается значение переменной экземпляра @response используя мемоизацию
-# - Метод который вовзращает значение переменной экземпляра @response
+# + Метод в котором присваивается значение переменной экземпляра @response используя мемоизацию и вовзращает значение переменной экземпляра @response
 
-# - Метод который получает данные с удаленного адреса(Url должен быть прописан в константе), парсит JSON в ruby
+# + Метод который получает данные с удаленного адреса(Url должен быть прописан в константе), парсит JSON в ruby
 #   url = 'https://api.apis.guru/v2/metrics.json'
 
-# - Метод получает Hash, фильтрует по определенным ключам и передает на запись в файл. Должен содержать белый список ключей,
+# + Метод получает Hash, фильтрует по определенным ключам и передает на запись в файл. Должен содержать белый список ключей,
 #   по этому белому списку нужно отфильтровать пары ключ-значение из полученного в аргументах Hash
 
-# - Метод call:
+# + Метод call:
 #   1 Вызвать метод, который получит json из удаленного сервера
 #   2 Вызов метода фильтрации
 #   3 Вызов метода записи в файл.
@@ -71,7 +74,7 @@ FileHandler.write(animal_names)
 
 # Все методы должны быть написаны как методы класса(self методы)
 
-
+# Написать тесты
 
 # Задача № 2
 
